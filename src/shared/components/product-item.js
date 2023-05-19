64883a6c3675d6677a50413e19a3de30d33f97b3
NguyenVanPhong2410 <phongham2410@gmail.com>
2023-05-20 00:38:05 +0700

    reactjsbuoi2 00:37 20/5/2023

---
+++ src/shared/components/product-item.js
@@ -0,0 +1,13 @@
+import { getImageProduct } from "../ultils.js";
+
+const ProductItem = ({item}) => {
+    return (
+    <div className="product-item card text-center">
+        <a href="#"><img src={getImageProduct(item.image)} /></a>
+        <h4><a href="#">{item.name}</a></h4>
+        <p>Giá Bán: <span>{item.price}đ</span></p>
+    </div>
+    )
+}
+
+export default ProductItem;
