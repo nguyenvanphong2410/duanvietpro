05b4732daf8fe99845f1db5509ed5c12e5e3d7da
NguyenVanPhong2410 <phongham2410@gmail.com>
2023-05-27 17:50:29 +0700

    reactjsbuoi3 Category 17:50 27/5/2023

--- src/services/Api.js
+++ src/services/Api.js
@@ -3,3 +3,18 @@ import Http from "./Http";
 export const getProducts = (config) => {
     return Http.get("products", config);
 }
+
+//API lấy về danh mục sản phẩm
+export const getCategories = (config) =>{
+    return Http.get("categories", config);
+}
+
+//Viết API để lấy tất cả sản phẩm tương ứng với category nhận được
+export const getProductsCategory = (id, config) => {
+    return Http.get(`categories/${id}/products`, config)
+} 
+
+// API lấy tên của danh mục sản phẩm, lấy ra cụ thể danh mục nào đó
+export const getCategory = (id, config) => {
+    return Http.get(`categories/${id}`, config)
+}
